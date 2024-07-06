3a9b329659fdfb0280f4ff7009482d3bf7c733c1
ashudevcodes <ashishprasad949@gmail.com>
2024-07-06 15:20:02 +0530

    fix: LSP not working

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,55 +1,55 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local lspconfig = require("lspconfig")
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-			lspconfig.lua_ls.setup({
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" },
-							disable = { "different-requires" },
-						},
-					},
-				},
-			})
+            lspconfig.tsserver.setup({
+                capabilities = capabilities,
+                init_options = {
+                    preferences = {
+                        disableSuggestions = true,
+                    },
+                },
+            })
 
-			lspconfig.gopls.setup({
-				filetypes = { "go", "gomod", "gowork", "gotmpl" },
-				settings = {
-					env = {
-						GOEXPERIMENT = "rangefunc",
-					},
-					formatting = {
-						gofumpt = true,
-					},
-				},
-			})
+            lspconfig.clangd.setup({
+                filetypes = { "c", "cpp" },
+            })
 
-			lspconfig.clangd.setup({
-				filetypes = { "c", "cpp" },
-			})
+            lspconfig.lua_ls.setup({
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim" },
+                            disable = { "different-requires" },
+                        },
+                    },
+                },
+            })
 
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-				init_options = {
-					preferences = {
-						disableSuggestions = true,
-					},
-				},
-			})
+            lspconfig.gopls.setup({
+                filetypes = { "go", "gomod", "gowork", "gotmpl" },
+                settings = {
+                    env = {
+                        GOEXPERIMENT = "rangefunc",
+                    },
+                    formatting = {
+                        gofumpt = true,
+                    },
+                },
+            })
 
-			-- lspconfig.tailwindcss.setup({
-			-- 	settings = {
-			-- 		includeLanguages = {
-			-- 			templ = "html",
-			-- 		},
-			-- 	},
-			-- })
-		end,
-	},
+            -- lspconfig.tailwindcss.setup({
+            -- 	settings = {
+            -- 		includeLanguages = {
+            -- 			templ = "html",
+            -- 		},
+            -- 	},
+            -- })
+        end,
+    },
 }
