fdaf9e3481e1e58598ffa6a1c17ba055156cc91b
ashudevcodes <ashishprasad949@gmail.com>
2025-08-26 14:25:40 +0530

    optimize

--- .config/awesome/veriable/globalVeriable.lua
+++ .config/awesome/veriable/globalVeriable.lua
@@ -5,8 +5,6 @@ editor = os.getenv("EDITOR") or "nvim"
 editor_cmd = terminal .. " -e " .. editor
 
 modkey = "Mod4"
--- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.spiral.dwindle,
+	awful.layout.suit.spiral.dwindle,
 }
--- }}}
