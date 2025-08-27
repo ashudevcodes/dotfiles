bd7f2d21522c0ca200f617c911ec1fd79362cac8
ashudevcodes <ashishprasad949@gmail.com>
2025-08-27 22:54:03 +0530

    chage on script so it use awesomedev rc.lua.new config while i debug

--- .config/awesome/scripts/run-with-xephyr.sh
+++ .config/awesome/scripts/run-with-xephyr.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-Xephyr :3 -ac -screen  1280x720 &
-XEPHYR_PID=$!
+Xephyr :3 -ac -br -noreset -screen 1280x720 &
 sleep 0.5
 
-DISPLAY=:3 awesome -c rc-devel.lua
+DISPLAY=:3 awesome -c ~/.config/awesome/awesomedev/rc.lua.new
+XEPHYR_PID=$!
 kill ${XEPHYR_PID}
