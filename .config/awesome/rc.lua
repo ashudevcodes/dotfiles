18adfc785a3062e9b5301ce40d312724c7c365cb
ashudevcodes <ashishprasad949@gmail.com>
2025-08-29 22:37:12 +0530

    add: garbage collection

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -4,27 +4,16 @@ require("awful.autofocus")
 require("awful.hotkeys_popup.keys")
 require("awful.remote")
 require("naughty")
-local gears = require("gears")
--- Create Custom theme
+
 require("./theme/custom_theme")
-
--- Glodal Veriable default terminal set and kind of stuff
 require("./veriable/globalVeriable")
-
--- Top Panel UI
 require("./ui/wibarPanal")
-
---  Key bind for mouse and keyboard
 require("./keybings/mouseAndKeyboardBings")
---
 require("./rules/rules")
---
 require("./signals/signals")
-
--- Startup Programs
 require("./programms/autoStartProgramms")
 
--- Run garbage collector regularly to prevent memory leaks
+local gears = require("gears")
 gears.timer({
 	timeout = 30,
 	autostart = true,
