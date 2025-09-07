d96c4f6e27e23ab568dfdbad93df2d769b90b78b
ashudevcodes <ashishprasad949@gmail.com>
2025-09-07 17:03:03 +0530

    add: write volume inc and dec script

--- .config/awesome/keybings/mouseAndKeyboardBings.lua
+++ .config/awesome/keybings/mouseAndKeyboardBings.lua
@@ -62,13 +62,13 @@ root.buttons(gears.table.join(
 globalkeys = gears.table.join(
 	awful.key({ modkey }, "s", hotkeys_popup.show_help, { description = "show help", group = "awesome" }),
 	awful.key({}, "XF86AudioRaiseVolume", function()
-		awful.spawn("wpctl set-volume @DEFAULT_AUDIO_SINK@ 4%+")
+		awful.spawn.with_shell("~/.config/awesome/scripts/volume.sh raise")
 	end),
 	awful.key({}, "XF86AudioLowerVolume", function()
-		awful.spawn("wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%-")
+		awful.spawn.with_shell("~/.config/awesome/scripts/volume.sh lower")
 	end),
 	awful.key({}, "XF86AudioMute", function()
-		awful.spawn("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle")
+		awful.spawn.with_shell("~/.config/awesome/scripts/volume.sh toggle_mute")
 	end),
 	awful.key({}, "XF86MonBrightnessUp", function()
 		awful.spawn("brightnessctl set +1%")
