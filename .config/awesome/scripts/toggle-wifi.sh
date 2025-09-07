be923fb0370f71a12de733cea9311e04b753d2e9
ashudevcodes <ashishprasad949@gmail.com>
2025-09-07 17:01:48 +0530

    fix: wifi toggle issue

--- .config/awesome/scripts/toggle-wifi.sh
+++ .config/awesome/scripts/toggle-wifi.sh
@@ -3,21 +3,20 @@
 status=$(iw wlan0 link)
 
 if echo "$status" | grep -q "Not connected."; then
-
-    sudo iwctl station wlan0 connect Home
-
+    rfkill toggle 1
     sleep 2
     new_status=$(iw wlan0 link)
 
     if echo "$new_status" | grep -q "Connected to"; then
-        notify-send "WiFi connected to Home!"
+        wifi_name=$(echo "$new_status" | awk -F': ' '/SSID:/{print $2; exit}')
+        notify-send "WiFi" "Connected to $wifi_name"
     else
         notify-send "Failed to connect to Home."
     fi
 
 elif echo "$status" | grep -q "Connected to"; then
-    sudo iwctl station wlan0 disconnect
-    notify-send "WiFi disconnected."
+    rfkill toggle 1
+    notify-send "WiFi Down"
 
 else
     notify-send "Unknown WiFi status: $status"
