67ec58f4976c83ba6ecda557c1773627e9dc6c78
ashudevcodes <ashishprasad949@gmail.com>
2025-09-15 22:38:42 +0530

    add: notification icon

--- .config/awesome/scripts/toggle-wifi.sh
+++ .config/awesome/scripts/toggle-wifi.sh
@@ -9,14 +9,12 @@ if echo "$status" | grep -q "Not connected."; then
 
     if echo "$new_status" | grep -q "Connected to"; then
         wifi_name=$(echo "$new_status" | awk -F': ' '/SSID:/{print $2; exit}')
-        notify-send "WiFi" "Connected to $wifi_name"
-    else
-        notify-send "Failed to connect to Home."
+        notify-send -i ~/.icon/wifi_connected.png "WiFi" "Connected to $wifi_name"
     fi
 
 elif echo "$status" | grep -q "Connected to"; then
 	rfkill toggle $(rfkill list | grep LAN | awk -F':'  '{print $1}')
-    notify-send "WiFi Down"
+    notify-send -i ~/.icon/wifi_disconnected.png "WiFi" "WiFi Down"
 
 else
     notify-send "Unknown WiFi status: $status"
