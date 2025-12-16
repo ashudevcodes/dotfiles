4c115b77adc8c87c15d075691105a18a7397a029
ashudevcodes <ashishprasad949@gmail.com>
2025-12-16 23:37:17 +0530

    remove icon from which key nvim plugins

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,13 +1,11 @@
 return {
   {
-    "folke/which-key.nvim",
-    { 'echasnovski/mini.nvim', version = false },
-    keys = { "<leader>", "<c-r>", "<c-w>", '"', "'", "`", "c", "v", "g" },
-    cmd = "WhichKey",
-    config = function(_, opts)
-      local wk = require('which-key')
-      wk.setup(opts)
-      wk.register({ "<leader>f", group = "Find" }, { prefix = "<leader>" })
-    end,
+	"folke/which-key.nvim",
+	keys = { "<leader>", "<c-r>", "<c-w>", '"', "'", "`", "c", "v", "g" },
+	cmd = "WhichKey",
+	config = function(_, opts)
+	  local wk = require('which-key')
+	  wk.setup(opts)
+	end,
   },
 }
