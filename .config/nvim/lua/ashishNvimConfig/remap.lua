fb3a8537205957bca97641de853b1490255f547b
ashudevcodes <ashishprasad949@gmail.com>
2025-12-28 18:00:40 +0530

    remove some keys remaps

--- .config/nvim/lua/ashishNvimConfig/remap.lua
+++ .config/nvim/lua/ashishNvimConfig/remap.lua
@@ -3,12 +3,10 @@ vim.g.maplocalleader = " "
 vim.keymap.set("n", "<leader>p", vim.cmd.Ex)
 vim.keymap.set("n", "<C-S>", ":update<CR>", { noremap = true, silent = true, desc = "Save file if modified" })
 
--- Remaps For Tabs Switching
-vim.keymap.set("n", "<A-l>", ":BufferNext<CR>")
-vim.keymap.set("n", "<A-h>", ":BufferPrevious<CR>")
+vim.keymap.set("n", "<A-l>", ":bnext<CR>")
+vim.keymap.set("n", "<A-h>", ":bprev<CR>")
 
--- Close the tab
-vim.keymap.set("n", "<A-c>", ":BufferClose<CR>")
+vim.keymap.set("n", "<A-c>", ":bd<CR>")
 
 vim.keymap.set("n", "<A-s>", "<cmd>vsplit<cr>")
 
@@ -19,7 +17,6 @@ vim.keymap.set({ "n", "t" }, "<C-j>", "<C-w>j")
 vim.keymap.set({ "n", "t" }, "<C-k>", "<C-w>k")
 vim.keymap.set({ "n", "t" }, "<C-l>", "<C-w>l")
 
--- Move lines up/down
 vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
 vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
