08982f8e719f5739aebf448cc6b5282c8f83577f
ashudevcodes <ashishprasad949@gmail.com>
2025-12-28 18:00:26 +0530

    fix for less mem user of lsp

--- .config/nvim/lua/plugins/lspConfig.lua
+++ .config/nvim/lua/plugins/lspConfig.lua
@@ -1,37 +1,60 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
+  {
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+	  "williamboman/mason.nvim",
+	  "williamboman/mason-lspconfig.nvim",
+	  "hrsh7th/cmp-nvim-lsp",
+	},
+	config = function()
+	  require("mason").setup()
 
-		event = { "BufReadPre", "BufNewFile" },
+	  local lspconfig = require("lspconfig")
+	  local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	  capabilities.offsetEncoding = { "utf-16" }
 
-		dependencies = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			"hrsh7th/cmp-nvim-lsp",
+	  require("mason-lspconfig").setup({
+		ensure_installed = { "lua_ls", "clangd" },
+		automatic_enable = true,
+		handlers = {
+		  function(server_name)
+			lspconfig[server_name].setup({
+			  capabilities = capabilities,
+  })
+  end,
+
+	["lua_ls"] = function()
+	  lspconfig.lua_ls.setup({
+		capabilities = capabilities,
+		settings = {
+		  Lua = {
+			workspace = {
+			  checkThirdParty = false, --
+			  maxPreload = 500,        --
+			  preloadFileSize = 500,   --
+			},
+			telemetry = { enable = false }, --
+		  },
 		},
+	  })
+	end,
 
-		config = function()
-			require("mason").setup()
-
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "clangd" },
-				autoamtic_enable = true,
-			})
-
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-			require("mason-lspconfig").setup({
-				function(server_name)
-					lspconfig[server_name].setup({
-						capabilities = capabilities,
-					})
-				end,
-			})
-		end,
-	},
-	{
-		"williamboman/mason.nvim",
-		cmd = "Mason",
-	},
+	["clangd"] = function()
+	  lspconfig.clangd.setup({
+		capabilities = capabilities,
+		cmd = {
+		  "clangd",
+		  "--background-index=false",
+		  "--limit-results=50",
+		  "--j=1",
+		  "--header-insertion=never",
+		  "--index-file-size=2000000",
+		},
+	  })
+	end,
+  },
+})
+end,
+},
 }
