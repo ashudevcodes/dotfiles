d83527e9fea10c4b399bc0d8af52fbc9a67a7b18
ashudevcodes <ashishprasad949@gmail.com>
2026-02-05 17:11:27 +0530

    fix: bat break terminal

--- .bashrc.d/leetcode.sh
+++ .bashrc.d/leetcode.sh
@@ -1,5 +1,5 @@
 leetcodeSession(){
   questionNumber=$(leetcode list | fzf | grep -oE "[1-9]+" | head -1)
   pickedQuestion=$(leetcode pick "$questionNumber")
-  echo "$pickedQuestion" | bat --file-name yaml 
+  echo "$pickedQuestion" | bat --file-name yaml --paging=always
 }
