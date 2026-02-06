10812004e54df187e5c1e6888cd755cd8382baa1
ashudevcodes <ashishprasad949@gmail.com>
2026-02-06 20:55:16 +0530

    fix: tmux auto open and attech setup

--- .bashrc.d/tmux.sh
+++ .bashrc.d/tmux.sh
@@ -1,4 +1,7 @@
-if [ -z "$(pgrep tmux)" ] && [ "$XDG_SESSION_TYPE" = "tty" ]; then
-  /bin/tmux
+# if [ -z "$(pgrep tmux)" ] && [ "$XDG_SESSION_TYPE" = "tty" ]; then
+#
+if [ -z "$(pgrep tmux)" ]; then
+  exec tmux
+elif [ -z "$TMUX" ] && [ "$(pgrep -c xterm)" -le 1 ]; then 
+  exec tmux attach
 fi
-
