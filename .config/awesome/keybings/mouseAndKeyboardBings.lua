758f0a1a93d67c66f00e9ae03d3c55b487b3cb18
ashudevcodes <ashishprasad949@gmail.com>
2026-02-19 00:34:49 +0530

    add title for screen timeout

--- .config/awesome/keybings/mouseAndKeyboardBings.lua
+++ .config/awesome/keybings/mouseAndKeyboardBings.lua
@@ -135,11 +135,11 @@ globalkeys = gears.table.join(
 		if dpms_enabled then
 			awful.spawn("xset s off -dpms")
 			dpms_enabled = false
-			naughty.notify({title ="Display", text = "Screen timeout disabled" })
+			naughty.notify({title ="Screen Timeout", text = "Disabled" })
 		else
 			awful.spawn("xset s on +dpms")
 			dpms_enabled = true
-			naughty.notify({title ="Display", text = "Screen timeout enabled" })
+			naughty.notify({title ="Screen Timeout", text = "Enabled" })
 		end
 	end, { description = "toggle screen timeout", group = "screen" }),
 	awful.key({ modkey }, "u", awful.client.urgent.jumpto, { description = "jump to urgent client", group = "client" }),
