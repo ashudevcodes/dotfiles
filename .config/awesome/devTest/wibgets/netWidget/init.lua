75fb378946381190b9dcf4dc1e186ddf28744af7
ashudevcodes <ashishprasad949@gmail.com>
2026-02-19 00:34:25 +0530

    dont wnat

--- .config/awesome/devTest/wibgets/netWidget/init.lua
+++
@@ -1,9 +0,0 @@
-local module_path = (...):match("(.+/)[^/]+$") or ""
-
-package.loaded.net_widgets = nil
-
-local net_widgets = {
-	wireless = require(module_path .. "netWidget/wireless"),
-}
-
-return net_widgets
