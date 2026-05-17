54db355c9d316e442c710ec8f113ec29494b3b11
ashudevcodes <ashudevcodes@gmail.com>
2026-05-17 18:49:20 +0530

    i dont think i need to start tmux when i first start terminal

--- .bashrc.d/tmux.sh
+++ .bashrc.d/tmux.sh
@@ -1,7 +1,7 @@
 # if [ -z "$(pgrep tmux)" ] && [ "$XDG_SESSION_TYPE" = "tty" ]; then
 #
-if [ -z "$(pgrep tmux)" ]; then
-  exec tmux
-elif [ -z "$TMUX" ] && [ "$(pgrep -c tmux)" -le 1 ]; then 
-  exec tmux attach
-fi
+#if [ -z "$(pgrep tmux)" ]; then
+#  exec tmux
+#elif [ -z "$TMUX" ] && [ "$(pgrep -c tmux)" -le 1 ]; then
+#  exec tmux attach
+#fi
