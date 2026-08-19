7281af8a96612d95c4e4310679d4f559fb1e8da4
ashudevcodes <ashudevcodes@gmail.com>
2026-08-20 00:26:38 +0530

    nvim: add plugin which make cursor animated

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -11,6 +11,10 @@ vim.pack.add({
 	{ src = "https://github.com/windwp/nvim-autopairs" },
 	{ src = "https://github.com/nvim-lua/plenary.nvim" },
 	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
+	{ src =	"https://github.com/sphamba/smear-cursor.nvim" },
+})
+
+require("smear_cursor").setup({
 })
 
 require("telescope").setup()
