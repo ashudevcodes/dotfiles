44024720200d15265e73f4032d7e1792f013eeb3
ashudevcodes <ashudevcodes@gmail.com>
2026-08-29 23:03:14 +0530

    fix: blank wallpaper not set on startup

--- .config/niri/scripts/wallpaper.sh
+++ .config/niri/scripts/wallpaper.sh
@@ -5,15 +5,16 @@ HOUR=$(date +%H)
 if [ "$HOUR" -ge 7 ] && [ "$HOUR" -lt 18 ]; then
   IMG="$HOME/assets/wallhaven-yqqwvd.jpg"
 elif [ "$HOUR" -ge 18 ] && [ "$HOUR" -lt 20 ]; then
-  IMG="$HOME/assets/future_place.jpg"
+  IMG="$HOME/assets/future_place.png"
 else
   IMG="$HOME/assets/portal_robot.png"
 fi
 
+FILE='/run/user/1000/wayland-1-awww-daemon.sock'
 # Wait until daemon is actually ready
-until pgrep -x awww-daemon >/dev/null; do
+until [ -S "$FILE" ]; do
   echo "waiting.."
   sleep 1
 done
 
-awww img --transition-fps 60 --transition-type random "$IMG"
+awww img --transition-fps 60 --transition-type random "$IMG" || exit 1
