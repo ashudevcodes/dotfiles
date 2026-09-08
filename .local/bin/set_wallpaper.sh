a274b922d71eef795271e79c826f57a0e1fbb827
ashudevcodes <ashudevcodes@gmail.com>
2026-09-08 22:15:40 +0530

    [binscript] change the location of wallpaper script to .local/bin

---
+++ .local/bin/set_wallpaper.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+HOUR=$(date +%H)
+
+if [ "$HOUR" -ge 7 ] && [ "$HOUR" -lt 18 ]; then
+  IMG="$HOME/assets/wallhaven-yqqwvd.jpg"
+elif [ "$HOUR" -ge 18 ] && [ "$HOUR" -lt 20 ]; then
+  IMG="$HOME/assets/the_wild_robot.jpg"
+else
+  IMG="$HOME/assets/portal_robot.png"
+fi
+
+FILE='/run/user/1000/wayland-1-awww-daemon.sock'
+# Wait until daemon is actually ready
+until [ -S "$FILE" ]; do
+  echo "waiting.."
+  sleep 1
+done
+
+awww img --transition-fps 60 --transition-type random "$IMG" || exit 1
